7d3efccd8a02503605ec0ce9e724d77e3eac13e7
tahuuhao <hao.th@zinza.com.vn>
2022-08-24 17:34:29 +0700

    Create Counter using redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './features/counter/Counter';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
