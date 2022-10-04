535ceec46923695ec14825dc1a98faa1e7123e4e
tahuuhao <111721025+tahuuhao@users.noreply.github.com>
2022-10-04 13:44:32 +0700

    Implement-Forgot-Password-Api (#23)
    * Implement-Forgot-Password-Api
    
    * Add Type Respone
    
    * Run prettier
    
    * Fix type

--- src/features/user/forgotPasswordSlice.ts
+++ src/features/user/forgotPasswordSlice.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../../app';
-import { fetchForgotPassword } from './userAPI';
+import { axioInstance } from '../../utils/request/httpRequest';
 export interface ForgotState {
   message: string;
   status: 'idle' | 'pending' | 'succeeded' | 'failed';
@@ -12,19 +12,37 @@ const initialState: ForgotState = {
   status: 'idle',
   loading: false
 };
-
+interface IPayload {
+  email: string;
+}
 export const forgotPasswordAsync = createAsyncThunk(
   'user/forgotPassword',
-  async (payload: string) => {
-    const response = await fetchForgotPassword(payload);
-    return response.data;
+  async (
+    payload: IPayload,
+    { rejectWithValue, fulfillWithValue }
+  ): Promise<string | unknown> => {
+    try {
+      const res = await axioInstance.post<{ message: string }>(
+        'forgot-password',
+        payload
+      );
+      return fulfillWithValue(res.data.message);
+    } catch (error: any) {
+      return rejectWithValue(error.response.data.message[0]);
+    }
   }
 );
 
 export const ForgotPasswordSlice = createSlice({
   name: 'forgotPassword',
   initialState,
-  reducers: {},
+  reducers: {
+    resetDefault: (state) => {
+      state.loading = false;
+      state.status = 'idle';
+      state.message = '';
+    }
+  },
   extraReducers: (builder) => {
     builder
       .addCase(forgotPasswordAsync.pending, (state) => {
@@ -32,17 +50,19 @@ export const ForgotPasswordSlice = createSlice({
         state.loading = true;
       })
       .addCase(forgotPasswordAsync.fulfilled, (state, action) => {
+        state.message = action.payload as string;
         state.status = 'succeeded';
         state.loading = false;
       })
       .addCase(forgotPasswordAsync.rejected, (state, action) => {
-        state.message = 'Not found email';
+        state.message = action.payload as string;
         state.status = 'failed';
         state.loading = false;
       });
   }
 });
 
+export const { resetDefault } = ForgotPasswordSlice.actions;
 export const selectLoading = (state: RootState) => state.forgotPassword.loading;
 export const selectState = (state: RootState) => state.forgotPassword;
 export default ForgotPasswordSlice.reducer;
